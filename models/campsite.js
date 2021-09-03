7c50d6ca0bf280fafa88e7d65094bcfcfbebf573
John Lyons <jwlyons2015@gmail.com>
2021-09-03 03:25:51 -0500

    Mongoose ODM Part 2 and 3 STILL CANNOT CONNECT TO SERVER

--- models/campsite.js
+++ models/campsite.js
@@ -1,6 +1,27 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true,
+        unique: true
+    }, 
+}, {
+    //allows mongoose to take care of automated timestamps (logging time of using and such)
+    timestamps: true
+});
+
 //2 arguments, first argument for schema, and other is for 2 other timestamps create app and update app
 const campsiteSchema = new Schema({
     name: {
@@ -11,7 +32,8 @@ const campsiteSchema = new Schema({
     description: {
         type: String,
         required: true
-    }
+    },
+    comments: [commentSchema]
 }, {
     timestamps: true
 });
--- models/index.js
+++ models/index.js
@@ -4,26 +4,45 @@ const url = 'mongodb://localhost:27017/nucampsite';
 //wrapper around mongoDB's driver node connect method
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
+    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
+
 
 connect.then(() => {
 
     console.log('Connected correctly to server');
 
-    const newCampsite = new Campsite({
+    //changed create
+    Campsite.create({
         name: 'React Lake Campground',
         description: 'test'
-    });
-
-    newCampsite.save()
+    })
     .then(campsite => {
         console.log(campsite);
-        return Campsite.find();
+        //make 2 updates to campsite document
+
+        return Campsite.fieByIdAndUpdate(campsite._id, {
+            $set: { description: 'Updated Test Document' }
+        }, {
+            new: true
+        });
     })
-    .then(campsites => {
-        console.log(campsites);
+    .then(campsite => {
+        console.log(campsite);
+
+        campsite.comments.push({
+            rating: 5,
+            text: 'What a magnificient view!',
+            author: 'Tinus Lorvaldes'
+        });
+        //returns campsite.save
+        return campsite.save();
+    })
+    .then(campsite => {
+        console.log(campsite);
         return Campsite.deleteMany();
     })
     .then(() => {
